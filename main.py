c2c49043c8123dff607701bbd9d1cff10c1ec64d
Dylan Landucci <quantix.dylan@gmail.com>
2026-01-05 12:09:16 -0500

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+import numpy as np
+
+file_path = librosa.ex('trumpet')
+y, sr = librosa.load(file_path)
+
+spectrogram = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128, fmax=8000)
+spectrogram_db = librosa.power_to_db(spectrogram, ref=np.max)
+
+plt.figure(figsize=(10, 4))
+librosa.display.specshow(spectrogram_db, x_axis='time', y_axis='mel', sr=sr, fmax=8000)
+plt.colorbar(format='%+2.0f dB')
+plt.title('This is what an AI "sees" when it listens to a Trumpet')
+plt.tight_layout()
+plt.show()
